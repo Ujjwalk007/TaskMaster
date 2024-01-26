dfaf6a98fc8fe459eca270ed217b33f1fed82968
Ujjwalk007 <104293762+Ujjwalk007@users.noreply.github.com>
2024-01-26 17:51:11 +0530

    MultiUser Env Added

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,6 +1,8 @@
 const mon = require('mongoose');
 
-mon.connect("mongodb+srv://UK:mongo%40uk@cluster0.awsgmde.mongodb.net/todoData");
+const connection = "mongodb+srv://UK:mongo%40uk@cluster0.awsgmde.mongodb.net/todoData";
+
+mon.connect(connection);
 console.log('Database Connected!!')
 
 const todoSchema = new mon.Schema({
